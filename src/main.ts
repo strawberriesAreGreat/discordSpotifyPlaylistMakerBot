f618d0623e2077779793850298b3d98e5b5a29b3
strw <strw@Cassandras-MacBook-Pro.local>
2023-10-25 08:55:05 -0400

    Updating dependency references

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,7 @@
 import DiscordClient from './middleware/discord/discordClient';
 import { eventListeners } from './middleware/discord/helpers/listener';
-import SpotifyAuth from './middleware/spotify/spotifyAuth';
-import server, {
-  requestAccessToken,
-} from './middleware/spotify/spotifyAuthCallback';
+import SpotifyAuth from './api/spotify/spotifyAuth';
+import server, { requestAccessToken } from './api/spotify/spotifyAuthCallback';
 
 //SpotifyAuth.getAccessToken();
 server.listen(process.env.REDIRECT_PORT, () => {
